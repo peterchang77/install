30452bde94cc0dc56938eea76ad4037841390600
Peter Chang <pchang077@gmail.com>
2020-01-21 21:43:14 +0000

    updates

--- linux/install-docker.sh
+++ linux/install-docker.sh
@@ -37,12 +37,29 @@
 # =======================================================
 # INSTALL NVIDIA-Container Toolkit 
 # =======================================================
+# --- Debian
 # sudo apt-get remove nvidia-docker2
 # sudo apt-get upgrade docker-ce
 # distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 # curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 # curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 # sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+# sudo systemctl restart docker
+
+# --- CentOS
+# DIST=$(sed -n 's/releasever=//p' /etc/yum.conf)
+# DIST=${DIST:-$(. /etc/os-release; echo $VERSION_ID)}
+# sudo rpm -e gpg-pubkey-f796ecb0
+# sudo gpg --homedir /var/lib/yum/repos/$(uname -m)/$DIST/*/gpgdir --delete-key f796ecb0
+# sudo gpg --homedir /var/lib/yum/repos/$(uname -m)/latest/nvidia-docker/gpgdir --delete-key f796ecb0
+# sudo gpg --homedir /var/lib/yum/repos/$(uname -m)/latest/nvidia-container-runtime/gpgdir --delete-key f796ecb0
+# sudo gpg --homedir /var/lib/yum/repos/$(uname -m)/latest/libnvidia-container/gpgdir --delete-key f796ecb0
+# sudo yum update
+# sudo yum remove nvidia-docker2
+# sudo yum upgrade docker-ce
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+# sudo yum install -y nvidia-container-toolkit
 # sudo systemctl restart docker
 
 # =======================================================
