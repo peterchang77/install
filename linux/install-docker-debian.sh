19b73b851d84fff77cfbc2389cf2aeee8e47bfe8
Ubuntu <ubuntu@ip-172-31-22-38.us-west-2.compute.internal>
2025-04-14 04:23:57 +0000

    updates

--- linux/install-docker-debian.sh
+++ linux/install-docker-debian.sh
@@ -13,8 +13,14 @@
 # =======================================================
 # INSTALL NVIDIA drivers 
 # =======================================================
-# ubuntu-drivers devices
-# sudo ubuntu-drivers autoinstall
+# Desktop: 
+# sudo ubuntu-drivers devices
+# sudo ubuntu-drivers install
+# =======================================================
+# Servers: 
+# sudo ubuntu-drivers list --gpgpu
+# sudo ubuntu-drivers install --gpgpu
+# =======================================================
 
 # =======================================================
 # INSTALL Docker
@@ -22,19 +28,25 @@
 echo "Install Docker runtime (y/n)?"
 read proceed
 if [ $proceed == "y" ]; then
+
+    # --- Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get -y install \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable"
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # --- Add repo 
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # --- Update
     sudo apt-get update
-    sudo apt-get -y install docker-ce
-    sudo systemctl restart docker
+
+    # --- Install
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 # =======================================================
@@ -44,21 +56,17 @@ echo "Install NVIDIA-Container Toolkit (y/n)?"
 read proceed
 if [ $proceed == "y" ]; then
 
+    # --- Add repo 
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+        && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+    # --- Update
     sudo apt-get update
 
-    # --- Remove nvidia-docker2 
-    if [ -x "$(command -v nvidia-docker)" ]; then
-        sudo apt-get remove nvidia-docker2
-    fi
-
-    # --- Upgrade Docker runtime
-    sudo apt-get upgrade docker-ce
-
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-    curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-    sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
-    sudo systemctl restart docker
+    # --- Install
+    sudo apt-get install -y nvidia-container-toolkit
 
 fi
 
@@ -68,6 +76,5 @@ fi
 echo "Install Docker-compose (y/n)?"
 read proceed
 if [ $proceed == "y" ]; then
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+    sudo apt-get install docker-compose-plugin
 fi
